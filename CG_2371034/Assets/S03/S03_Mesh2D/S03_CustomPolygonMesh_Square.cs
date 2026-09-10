16aa6e6db7a8348f30ffd2b50758d58cf0b5823d
PSH <psihyeon0921@gmail.com>
2026-09-10 11:09:01 +0900

    feat: script에 사각형용 벡터 추가

--- CG_2371034/Assets/S03/S03_Mesh2D/S03_CustomPolygonMesh_Square.cs
+++ CG_2371034/Assets/S03/S03_Mesh2D/S03_CustomPolygonMesh_Square.cs
@@ -7,8 +7,17 @@
       {
           // TODO 1: 원하는 다각형의 정점 좌표를 채우세요 (최소 4개)
           Vector3[] vertices = new Vector3[]
-          {
-              // 예: new Vector3(0f, 1f, 0f),
+          {     //사각형용 벡터
+                new Vector3(0f, 0f, 0f), // 0
+                new Vector3(1f, 0f, 0f), // 1
+                new Vector3(1f, 1f, 0f), // 2
+                new Vector3(0f, 1f, 0f), // 3
+                new Vector3(0f, 0f, 1f), // 4
+                new Vector3(1f, 0f, 1f), // 5
+                new Vector3(1f, 1f, 1f), // 6
+                new Vector3(0f, 1f, 1f), // 7
+
+
           };
 
           // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
